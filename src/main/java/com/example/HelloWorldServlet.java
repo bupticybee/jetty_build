d3436099b37451d8197dec3a5e85ab4f2fffd8cc
icybee <icybee@yeah.net>
2016-11-06 15:53:15 +0800

    modify code to support new interface

--- src/main/java/com/example/HelloWorldServlet.java
+++ src/main/java/com/example/HelloWorldServlet.java
@@ -5,18 +5,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import com.chenzehe.runjar.Run;
+import com.icyplat.app.IcyplatApi;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Enumeration;
+import com.google.gson.Gson;
 
 @WebServlet(urlPatterns = {"/*"}, loadOnStartup = 1)
 public class HelloWorldServlet extends HttpServlet 
 {
-  @Override 
-  public void doGet(HttpServletRequest request, HttpServletResponse response) 
-      throws IOException
-  {
-      String[] param = {""};
-      Run run = new Run();
-      run.main(param);
-      response.getOutputStream().print("Hello World");
-  }
+    IcyplatApi icyplatapi = new IcyplatApi();
+    @Override 
+    public void doGet(HttpServletRequest request, HttpServletResponse response) 
+        throws IOException
+    {
+        Enumeration<String> parameterNames = request.getParameterNames();
+        HashMap<String, String> map = new HashMap<String, String>();
+        while (parameterNames.hasMoreElements()) {
+            String key = (String) parameterNames.nextElement();
+            String val = request.getParameter(key);
+            map.put(key, val);
+        }
+	HashMap<String, String> res = icyplatapi.recvMessage(map);
+	//String out = (String)res.get("name");
+	Gson gson = new Gson();
+	String out = gson.toJson(res);
+        response.getOutputStream().print(out);
+    }
 }
